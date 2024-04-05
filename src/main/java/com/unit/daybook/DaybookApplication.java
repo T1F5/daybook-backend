76ab10a9bbaf81b96207190afa07b320c0f661dc
ybchar <uiurihappy@sju.ac.kr>
2024-04-06 01:44:49 +0900

    init Project

---
+++ src/main/java/com/unit/daybook/DaybookApplication.java
@@ -0,0 +1,13 @@
+package com.unit.daybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaybookApplication.class, args);
+	}
+
+}
