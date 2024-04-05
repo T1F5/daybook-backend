d813239917abc9a376243e4a4d62a3e4eab7edfe
ybchar <uiurihappy@sju.ac.kr>
2024-04-06 03:01:34 +0900

    feat: 회원 엔티티 구현(1차)

---
+++ src/main/java/com/unit/daybook/domain/member/repository/MemberRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.unit.daybook.domain.member.repository;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+}
